1f5541224c872b8f58171c58ebf7dd7b3b9c030b
汪广鑫 <2401881997@qq.com>
2021-01-08 13:24:33 +0800

    移动数组

---
+++ python code/189_rotate.py
@@ -0,0 +1,43 @@
+给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+
+示例 1:
+
+输入: [1,2,3,4,5,6,7] 和 k = 3
+输出: [5,6,7,1,2,3,4]
+解释:
+向右旋转 1 步: [7,1,2,3,4,5,6]
+向右旋转 2 步: [6,7,1,2,3,4,5]
+向右旋转 3 步: [5,6,7,1,2,3,4]
+示例 2:
+
+输入: [-1,-100,3,99] 和 k = 2
+输出: [3,99,-1,-100]
+解释: 
+向右旋转 1 步: [99,-1,-100,3]
+向右旋转 2 步: [3,99,-1,-100]
+说明:
+
+尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+要求使用空间复杂度为 O(1) 的 原地 算法。
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k % n
+        temp = nums[n-k:]
+        for i in range(n-k):
+            nums[n-i-1] = nums[n-i-k-1]
+        nums[0:k] = temp
+
+    def rotate_2(self, nums: List[int], k: int) -> None:
+    	'''
+    	在数据结构的书上见过这种题：大概是三次 逆置 操作
+    	'''
+        n = len(nums)
+        k %= n
+        nums[:] = nums[::-1]
+        nums[:k] = nums[:k][::-1]
+        nums[k:] = nums[k:][::-1]
