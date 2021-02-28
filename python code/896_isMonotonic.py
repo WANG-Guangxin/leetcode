ab4f9d7d0b32ef2d525f64236ae1fd2dca5919bf
WANG-Guangxin <w-gx@qq.com>
2021-02-28 17:14:19 +0800

    单调数列

---
+++ python code/896_isMonotonic.py
@@ -0,0 +1,56 @@
+896. 单调数列
+
+如果数组是单调递增或单调递减的，那么它是单调的。
+
+如果对于所有 i <= j，A[i] <= A[j]，那么数组 A 是单调递增的。 如果对于所有 i <= j，A[i]> = A[j]，那么数组 A 是单调递减的。
+
+当给定的数组 A 是单调数组时返回 true，否则返回 false。
+
+ 
+
+示例 1：
+
+输入：[1,2,2,3]
+输出：true
+示例 2：
+
+输入：[6,5,4,4]
+输出：true
+示例 3：
+
+输入：[1,3,2]
+输出：false
+示例 4：
+
+输入：[1,2,4,5]
+输出：true
+示例 5：
+
+输入：[1,1,1]
+输出：true
+ 
+
+提示：
+
+1 <= A.length <= 50000
+-100000 <= A[i] <= 100000
+
+
+class Solution:
+    def isMonotonic(self, A: List[int]) -> bool:
+        n = len(A)
+        if n < 3:return True
+        i = 1
+        while i < n and A[i] == A[i-1]:
+            i += 1
+        if i == n:return True
+        tag0 = 1 if A[i-1] < A[i] else 0
+        while i < n-1:
+            if A[i] == A[i+1]:
+                i += 1
+                continue
+            tag1 = 1 if A[i] < A[i+1] else 0
+            if tag0 != tag1:
+                return False
+            i += 1
+        return True     
