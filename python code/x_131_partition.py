d73d284a44436b46cb08a6a0f8e72c69237f77b2
WANG-Guangxin <w-gx@qq.com>
2021-03-07 11:43:17 +0800

    分割回文串 回溯法

---
+++ python code/x_131_partition.py
@@ -0,0 +1,44 @@
+131. 分割回文串
+
+给定一个字符串 s，将 s 分割成一些子串，使每个子串都是回文串。
+
+返回 s 所有可能的分割方案。
+
+示例:
+
+输入: "aab"
+输出:
+[
+  ["aa","b"],
+  ["a","a","b"]
+]
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+    	'''
+    	回溯法  直接copy
+    	'''
+        n = len(s)
+        f = [[True] * n for _ in range(n)]
+
+        for i in range(n - 1, -1, -1):
+            for j in range(i + 1, n):
+                f[i][j] = (s[i] == s[j]) and f[i + 1][j - 1]
+
+        ret = list()
+        ans = list()
+
+        def dfs(i: int):
+            if i == n:
+                ret.append(ans[:])
+                return
+            
+            for j in range(i, n):
+                if f[i][j]:
+                    ans.append(s[i:j+1])
+                    dfs(j + 1)
+                    ans.pop()
+
+        dfs(0)
+        return ret
+
