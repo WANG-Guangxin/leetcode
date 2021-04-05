72ea5ef9f7ea5181bef263d844a6105baedb54de
WANG-Guangxin <w-gx@qq.com>
2021-04-05 13:08:16 +0800

    88. 合并两个有序数组   简单题 墨迹了好久  太不合格了

---
+++ python code/88_merge.py
@@ -0,0 +1,46 @@
+88. 合并两个有序数组
+
+给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+
+初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 的空间大小等于 m + n，这样它就有足够的空间保存来自 nums2 的元素。
+
+ 
+
+示例 1：
+
+输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+输出：[1,2,2,3,5,6]
+示例 2：
+
+输入：nums1 = [1], m = 1, nums2 = [], n = 0
+输出：[1]
+ 
+
+提示：
+
+nums1.length == m + n
+nums2.length == n
+0 <= m, n <= 200
+1 <= m + n <= 200
+-109 <= nums1[i], nums2[i] <= 109
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i = j = cnt = 0
+        while j < n:
+            if nums1[i] >= nums2[j] and i < cnt + m:
+                for x in range(m+n-1,i,-1):
+                    nums1[x] = nums1[x-1]
+                nums1[i] = nums2[j]
+                j += 1
+                cnt += 1
+            elif i == cnt + m:
+                nums1[i] = nums2[j]
+                i += 1
+                j += 1
+                cnt += 1
+            else:
+                i += 1  
